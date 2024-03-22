1b811432359646340a1c3cdaa73a0859d3a5de72
PaoloRollo <paolo.rollo1997@gmail.com>
2024-03-22 01:35:57 +0100

    feat: update logic with storage of last mints and frame

--- src/cron.ts
+++ src/cron.ts
@@ -54,24 +54,58 @@ export const fetchAndSendTrendingMints = async (timeFrame: TimeFrame) => {
     // Send trending mints to user
     await conversation.send("🚀 New mints are trending! Check them out now.");
 
+    // Check if the user has already received the first send
+    const firstSend = await redisClient.get(
+      `first-send-${conversation.peerAddress}`
+    );
+    // If use has not received the first send, send 5 mints, else send 2
+    const amount = !firstSend ? 5 : 2;
+
+    if (!firstSend) {
+      // Toggle the first send for the user
+      await redisClient.set(`first-send-${conversation.peerAddress}`, "true");
+    }
+    // Get the last mints sent to the user
+    const lastMintsSent = await redisClient.get(
+      `last-mints-${conversation.peerAddress}`
+    );
+    // Parse the last mints sent JSON array (or return an empty array)
+    const parsedLastMints = lastMintsSent
+      ? (JSON.parse(lastMintsSent) as string[])
+      : [];
+
+    // Filter the mints to send to the user
+    const mintsToSend = trendingMints.filter((mint) => {
+      if (!lastMintsSent || lastMintsSent.length === 0) return true;
+      return !parsedLastMints.includes(mint.address!);
+    });
+
+    // Skip if no mints to send
+    if (mintsToSend.length === 0) {
+      continue;
+    }
+
+    // Get the slice
+    const mintsToSendSlice = mintsToSend.slice(0, amount);
+
+    // Store the last mints for the user
+    const mintsToSendAddresses = mintsToSendSlice.map((mint) => mint.address!);
+    await redisClient.set(
+      `last-mints-${conversation.peerAddress}`,
+      JSON.stringify(
+        Array.from(new Set([...mintsToSendAddresses, ...parsedLastMints]))
+      )
+    );
     // TODO:
     // instead of sending the message in text format, for each trending mint send a message with a custom frame link
     // https://mint.builders.garden/<chain>/<contractAddress> ---> we need to create this frame
     // the frame should have the mint's image, name, and number of trending mints
     // a single button that redirects to https://zora.co/collect/<chain>:<contractAddress>
-    await conversation.send(
-      trendingMints
-        // TODO: filter out trending mints that where already sent in previous messages
-        .filter(Boolean)
-        .slice(0, 5)
-        .map((mint) =>
-          [
-            mint.token!.name,
-            mint.token?.tokenNfts![0].contentValue?.image?.medium,
-            mint.criteriaCount,
-          ].join(" - ")
-        )
-        .join("\n")
+
+    await Promise.all(
+      mintsToSendSlice.map((mint) =>
+        conversation.send(`https://mint.builders.garden/base/${mint.address}`)
+      )
     );
   }
 };
