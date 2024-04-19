9a94199c0fe462653f5f662c43bed5def2a1283a
fabri <fguespe@gmail.com>
2024-04-18 22:25:31 -0300

    update

--- src/airstack/airstack.ts
+++ src/airstack/airstack.ts
@@ -7,7 +7,7 @@ import {
 } from "./airstack-types";
 import { getRedisClient } from "../lib/redis.js";
 
-init(process.env.AIRSTACK_API_KEY as string);
+//init(process.env.AIRSTACK_API_KEY as string);
 
 export const TRENDING_MINTS_QUERY_BASE =
   /* GraphQL */
@@ -125,6 +125,7 @@ export const fetchTrendingMints = async (
   criteria: TrendingMintsCriteria
 ) => {
   // Temporary fake data
+  // Extended fake data
   const fakeData = [
     {
       address: "0x8609fc45c17cfaed5710e998a98cfe634cee05aa",
@@ -154,7 +155,40 @@ export const fetchTrendingMints = async (
       address: "0xf9eb4885b3ebda09b28ac4e9630db13adb0ef59e",
       criteriaCount: 85,
     },
+    {
+      address: "0x870381bba5ae811e6c35c3088378b5f5afb8f9b3",
+      criteriaCount: 1825,
+    },
+    {
+      address: "0x2ebdcabd72265d21980ac53dd282ea54d7b172a2",
+      criteriaCount: 40,
+    },
+    {
+      address: "0x931f3a41078725e85c8fc0bf8204f277d3ccbd11",
+      criteriaCount: 23,
+    },
+    {
+      address: "0x366e0ba310ed1b703aca4a421cbe53a5655ca634",
+      criteriaCount: 12,
+    },
+    {
+      address: "0x81cdf5e6e3f26a8b83814366e3c397477d3b8a0a",
+      criteriaCount: 43,
+    },
+    {
+      address: "0xd2c2d1ac51ba7d1ec74da90b07b0fc3fb55294ec",
+      criteriaCount: 1063,
+    },
+    {
+      address: "0x05490ae02f71d2f3d1c96a7e62b44a1b2348c66b",
+      criteriaCount: 45,
+    },
+    {
+      address: "0x9252300cfb276d12ccd9947860d26f091decc9a2",
+      criteriaCount: 57,
+    },
   ];
+
   return fakeData;
   /*
   const { data, error }: QueryResponse = await fetchQuery(
